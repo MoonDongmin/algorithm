d8937a6823ca64f77fbce1690cd1fcd688c17c93
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-02-19 16:30:59 +0900

    코드 수정

--- chapter02/binarySearch.js
+++ chapter02/binarySearch.js
@@ -12,3 +12,26 @@ function location(low, high) {
     else return location(mid + 1, high);
   }
 }
+
+
+function binarySearch(target, arr) {
+  let answer;
+
+  // 낮은 수 -> 높은 수 정렬
+  arr.sort((a, b) => a - b);
+  let lt = 0;
+  let rt = arr.length - 1;
+
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) {
+      rt = mid - 1;
+    } else if (arr[mid] < target) {
+      lt = mid + 1;
+    }
+  }
+  return answer;
+}
--- chapter02/mergesort2.js
+++ chapter02/mergesort2.js
@@ -1,13 +1,50 @@
-let s;
-let n;
+// let s;
+// let n;
+//
+// function merge2(low, mid, high) {
+//   let i = low;
+//   let j = mid + 1;
+//   let k = low;
+//   let u;
+//
+//   while (i <= mid && i <= high) {
+//     if (s[i] < s[j]) {
+//       u[k] = s[i];
+//       i++;
+//     } else {
+//       u[k] = s[j];
+//       j++;
+//     }
+//     k++;
+//     if(i>mid){
+//       [s[j],u[k]] = [s[high],u[high]]
+//     }else{
+//
+//     }
+//   }
+// }
+//
+// function mergesort2(low, high) {
+//   let mid;
+//
+//   if (low < high) {
+//     mid = Math.floor((low + high) / 2);
+//     mergesort2(low, mid);
+//     mergesort2(mid + 1, high);
+//     merge2(low, mid, high);
+//   }
+// }
+
+
+let s = [2, 5, 1, 12, 9, 3, 8];
+let u = Array(s.length);
 
 function merge2(low, mid, high) {
   let i = low;
   let j = mid + 1;
   let k = low;
-  let u;
 
-  while (i <= mid && i <= high) {
+  while (i <= mid && j <= high) {
     if (s[i] < s[j]) {
       u[k] = s[i];
       i++;
@@ -16,21 +53,33 @@ function merge2(low, mid, high) {
       j++;
     }
     k++;
-    if(i>mid){
-      [s[j],u[k]] = [s[high],u[high]]
-    }else{
+  }
 
-    }
+  while (i <= mid) {
+    u[k] = s[i];
+    i++;
+    k++;
+  }
+
+  while (j <= high) {
+    u[k] = s[j];
+    j++;
+    k++;
+  }
+
+  for (let l = low; l <= high; l++) {
+    s[l] = u[l];
   }
 }
 
 function mergesort2(low, high) {
-  let mid;
-
   if (low < high) {
-    mid = Math.floor((low + high) / 2);
+    let mid = Math.floor((low + high) / 2);
     mergesort2(low, mid);
     mergesort2(mid + 1, high);
     merge2(low, mid, high);
   }
 }
+
+mergesort2(0, s.length - 1);
+console.log(s); // [1, 2, 3, 5, 8, 9, 12]
